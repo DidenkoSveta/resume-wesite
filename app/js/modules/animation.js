a7f6c3dd2548874ccf2eab43c0087b7776a4ae69
DidenkoSveta <garpia.lana@gmail.com>
2023-12-09 03:30:56 +0400

    animation about me and soft skills
    animation of the appearance of blocks in the about me and soft skills section

--- app/js/modules/animation.js
+++ app/js/modules/animation.js
@@ -1,3 +1,4 @@
+// Анимация печати заголовка h1
 document.addEventListener('DOMContentLoaded', function() {
    const typingElement = document.querySelector('.hero__title-name'); // Элемент, в котором будет эффект печатания
    const textToType = 'Svetlana,<br>HTML-Coder'; // Текст для печатания с HTML тегом для переноса строки
@@ -14,10 +15,30 @@ document.addEventListener('DOMContentLoaded', function() {
          typedText += textToType[charIndex++];
        }
        typingElement.innerHTML = typedText; // Используем innerHTML для вставки HTML-содержимого
-       setTimeout(typeCharacter, 150); // Задержка перед печатанием следующего символа
+       setTimeout(typeCharacter, 60); // Задержка перед печатанием следующего символа
      }
    }
  
    typeCharacter(); // Начать эффект печатания
  });
+ 
+
+//Анимация появления карточек навыков
+document.addEventListener("DOMContentLoaded", function() {
+   let observer = new IntersectionObserver((entries, observer) => {
+     entries.forEach(entry => {
+       if (entry.isIntersecting) {
+         entry.target.classList.add("in-view");
+         observer.unobserve(entry.target);
+       }
+     });
+   }, {threshold: 0.1});
+ 
+   // Наблюдаем за элементами .skills__item, .about-me и .education
+   const elementsToAnimate = document.querySelectorAll('.skills__item, .about-me, .education, .languages, .soft-skills');
+   elementsToAnimate.forEach(item => {
+     observer.observe(item);
+   });
+ });
+ 
  
